99d2bc308cddbc6bf4867aa28059e190c78a59c3
mmck328 <edamame1010@hotmail.co.jp>
2020-01-24 21:52:46 +0900

    implement

---
+++ src/components/Chart.vue
@@ -0,0 +1,36 @@
+<script>
+import { Scatter, mixins } from "vue-chartjs";
+export default {
+  mixins: [Scatter, mixins.reactiveData],
+  data: () => {
+    return {
+      options: {
+        elements: {
+          line: {
+            tension: 0,
+            fill: false
+          },
+        },
+        scales: {
+          xAxes: [{
+            type: 'time'
+          }]
+        },
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    };
+  },
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    }
+  },
+  watch: {
+    charData: function() {
+      this.renderChart();
+    }
+  }
+};
+</script>
---
+++ src/components/Main.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-container>
+    <v-row text-center wrap>
+      <v-col>
+        <v-combobox
+          v-model="users"
+          item-disabled
+          chips
+          clearable
+          label="Input AtCoder usernames"
+          multiple
+          solo
+        >
+          <template v-slot:selection="{ attrs, item, select, selected }">
+            <v-chip
+              v-bind="attrs"
+              close
+              @click:close="remove(item)"
+            >
+              <strong>{{ item }}</strong>
+            </v-chip>
+          </template>
+        </v-combobox>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <div id="chart">
+          <Chart v-if="!chartData.datasets.isEmpty" :chart-data="chartData" />
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+#chart {
+  width: 80vw;
+}
+</style>
+
+<script>
+import axios from "axios";
+import Chart from "./Chart";
+
+export default {
+  name: "Main",
+  components: {
+    Chart
+  },
+  data: () => {
+    return {
+      users: [],
+      chartData: {
+        datasets: []
+      }
+    };
+  },
+  methods: {
+    update_datasets() {
+      let promises = this.users.map(async (user, index) => {
+        let response = await axios.get(
+          "https://any-proxy.herokuapp.com/https://atcoder.jp/users/" +
+            user +
+            "/history/json"
+        );
+        return {
+          label: user,
+          data: response.data.map(contest => {
+            return {
+              x: Date.parse(contest.EndTime),
+              y: contest.NewRating
+            };
+          }),
+          borderColor: "hsl(" + 145 * index + ", 60%, 60%)",
+          showLine: true
+        };
+      });
+      Promise.all(promises).then(result => {
+        this.chartData = { datasets: result };
+      });
+    },
+    remove(user) {
+      this.users.splice(this.users.findIndex((item) => { item == user }), 1);
+    }
+  },
+  watch: {
+    users: function() {
+      this.update_datasets();
+    }
+  },
+  created: function() {
+    this.update_datasets();
+  }
+};
+</script>
